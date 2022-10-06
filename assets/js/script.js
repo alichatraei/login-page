0c0b31bc0dfd163b9f69c5591deceaa559828b7a
Ali chatraei <avida.chatraee2001@yahoo.com>
2022-10-06 12:18:49 +0330

    complete login-page

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// const usernameInputElement = document.querySelector("#username"),
+//   regex = /[a-zA-Z]/g;
+// function validateCurrencyPattern(text) {
+//   var matchedString = text.match(regex);
+//   if (matchedString) {
+//     // prevValue =
+//     //   matchedString[1] +
+//     //   (matchedString[2] ? matchedString[2].replace(",", ".") : "");
+//     console.log(matchedString);
+//     return prevValue;
+//   } else {
+//     return prevValue;
+//   }
+// }
+// usernameInputElement.addEventListener("keypress", ({ target: { value } }) => {
+//   usernameInputElement.value = validateCurrencyPattern(value);
+// });
